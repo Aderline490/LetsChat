b28e6553902b2a77a663ccccb0b4c191374a70c9
Gashugi Aderline <75659434+Aderline490@users.noreply.github.com>
2021-11-16 11:42:28 +0200

    final commit

---
+++ public/js/main.js
@@ -0,0 +1,77 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages =document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+//Get username and the room from the url
+const { username, room } =Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+});
+
+const socket = io();
+
+//Join chatroom
+socket.emit('joinRoom',{username,room})
+
+
+// Get room and users
+socket.on('roomUsers', ({room,users})=>{
+    outputRoomName(room);
+    outputUsers(users);
+})
+
+
+
+//Message from server
+socket.on('message',message=>{
+    console.log(message);
+    outputMessage(message);
+
+    //Scroll down 
+    chatMessages.scrollTop = chatMessages.scrollHeight
+});
+
+
+
+//Message submit
+
+chatForm.addEventListener('submit', (e)=>{
+    e.preventDefault(); // prevents default behaviour of submiting the form to a file
+
+    //Get the message text
+    const msg= e.target.elements.msg.value; // msg here is the id of the message
+    
+    //Emit the message to the server 
+    socket.emit('chatMessage',msg);
+
+    //Clear input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+})
+
+
+
+//Output message to DOM
+
+function outputMessage(message){
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+    <p class="text">
+        ${message.text}
+    </p>`
+    document.querySelector('.chat-messages').appendChild(div)
+}
+
+//Add room to DOM
+function outputRoomName(room){
+    roomName.innerText = room;
+
+}
+
+//Add users to DOM
+function outputUsers(users){
+    userList.innerHTML = `
+        ${users.map(user=> `<li>${user.username}</li>`).join('')}
+    `;
+}
